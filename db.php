b4758b076c810ca0bb7767845950f9c4b9effb24
Ben Keener <keener4christ@gmail.com>
2020-11-17 18:54:48 -0500

    Removed postgres URI

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 
 $db = (function() {
-  $parts = (parse_url(getenv('DATABASE_URL') ?: 'postgres://osaxydmvbsxvdn:a4abf5a952a69a534ef4a7c6593519f6eba0f16cfd9dc216dd4e6b700610143f@ec2-52-207-124-89.compute-1.amazonaws.com:5432/d6s648r538kctj'));
+  $parts = (parse_url(getenv('DATABASE_URL')));
   extract($parts);
   $path = ltrim($path, "/");
 
