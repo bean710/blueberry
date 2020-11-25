b3be1cad3bb18e486455b305f8dceb72ef7ff989
Ben Keener <keener4christ@gmail.com>
2020-11-25 16:56:57 -0500

    Updated to store postgame quiz data in database

--- postquiz_store.php
+++ postquiz_store.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 
+include 'db.php';
+
 /*
 $dt1=explode("_", $_GET['dt1']);
 $dt2=explode("_", $_GET['dt2']);
@@ -12,6 +14,10 @@ $dt2=$_GET['dt2'];
 $ra1=$_GET['ra1']; // Should be a single value like: "5:5" as question:answer
 $ra2=$_GET['ra2'];
 
+$pq = $db->prepare('INSERT INTO postquiz VALUES(:subjnum, :dt1, :ra1, :dt2, :ra2)');
+$pq->execute(['subjnum' => $_SESSION['subjnum'], 'dt1' => $dt1, 'ra1' => $ra1, 'dt2' => $dt2, 'ra2' => $ra2]);
+
+/*
 $pqdat=$_SESSION['subjnum'].",".$dt1.",".$ra1.",".$dt2.",".$ra2."\n";
 
 echo $pqdat;
@@ -19,5 +25,6 @@ echo $pqdat;
 $fh = fopen("./results/postquiz_results.txt", 'a');
 fwrite($fh, $pqdat);
 fclose($fh);
+*/
 
 ?>
